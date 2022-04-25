b7f0fac2d8dd3b1e3a8aaae886617810bb2877a8
cesartidrick <cesartidrick@gmail.com>
2022-04-25 12:05:00 -0600

    commit tree 4d02edbfff3d3b00c3416767d5d08c0b1382eb8c

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42) {
+        return blocks - 42;
+    } else {
+        return 42-blocks
+    }    
+}
+
+function distanceFromHqInFeet(feet) {
+    return distanceFromHqInBlocks(feet) * 264;
+   }
+
+function distanceTravelledInFeet(start, destination) {
+    if (destination > start) {
+        return (destination - start) * 264;
+    } else {
+        return (start-destination) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0;
+    } else if (distanceTravelledInFeet(start, destination) >= 400 && distanceTravelledInFeet(start, destination) < 2000) {
+        return .02 * (distanceTravelledInFeet(start, destination) - 400);
+    } else if (distanceTravelledInFeet(start, destination) >= 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far'
+    }
+}
+
+console.log(calculatesFarePrice(50, 58));
